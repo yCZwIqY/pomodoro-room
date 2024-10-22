c42e52d8c9624c6cc112f918112a980b4d13879e
jiyoon <ljy10499@gmail.com>
2024-10-22 20:56:57 +0900

    알람 권한 요청

--- src/App.tsx
+++ src/App.tsx
@@ -23,15 +23,25 @@ function App() {
       initFurnitureData();
     }
 
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.getRegistrations().then((registrations) => {
-          registrations.forEach((registration) => {
-            registration.unregister();
-          });
-          navigator.serviceWorker.register('/service-worker.js');
-        });
+    if (Notification.permission === 'default') {
+      alert('원할한 서비스 사용을 위해 알림권한을 활성화 해주세요!')
+      Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+          console.log('알림 권한이 부여되었습니다.');
+        } else if (permission === 'denied') {
+          console.log('알림 권한이 거부되었습니다.');
+        }
       });
+    }
+
+    if ('serviceWorker' in navigator) {
+      if (!navigator.serviceWorker.controller) {
+        navigator.serviceWorker.register('/service-worker.js').then(() => {
+          console.log('서비스워커 등록');
+        }).catch(error => {
+          console.error('서비스워커 등록 실패:', error);
+        });
+      }
     }
 
     return () => {
